954443e2b3806872751bb6a1e89ca7e53e640820
Tristan Kobusch <tristan.kobusch@gmail.com>
2021-02-05 22:44:32 +0100

    current non working poisson

--- discretization/poisson.h
+++ discretization/poisson.h
@@ -9,71 +9,79 @@
 #include <utility>
 
 template<typename T>
-std::pair<SparseMatrix<T>, Vector<T>> assemble_poisson_matrix(const RegularGrid &grid,
-                                                              const std::function<T(const Point &)> &rhs_function,
+std::pair<SparseMatrix<T>, Vector<T>> assemble_poisson_matrix(const RegularGrid& grid,
+                                                              const std::function<T(const Point&)>& rhs_function,
                                                               const std::function<T(
-                                                                  const Point &)> &boundary_function) {
-    //std::vector<typename SparseMatrix<T>::triplet_type> v;
-    const int max_neighbor = 2*space_dimension;
-    std::function<int(int)> nnz_per_index = [&grid,max_neighbor](int i)
-    {
-      return 1+ (grid.is_boundary_node(i) ? 0 : max_neighbor);
+                                                                  const Point&)>& boundary_function)
+{
+    const int max_neighbor = 2 * space_dimension;
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::function<int(int)> nnz_per_index = [&grid, max_neighbor, rank](int i) {
+        return 1 + (grid.is_boundary_node(grid.partition().to_global_index(i)) ? 0 : max_neighbor);
     };
-    Vector<T> b(grid.number_of_nodes());
-    SparseMatrix<T> m(grid.number_of_nodes(), grid.number_of_nodes(), nnz_per_index);
+    Vector<T> b(grid.partition().local_size());
+    SparseMatrix<T> m(grid.partition().local_size(), grid.partition().local_size(), nnz_per_index);
     int entry_counter;
-    for (int i = 0; i < grid.number_of_nodes(); ++i) {
+    for(int i = 0; i < grid.partition().local_size(); ++i)
+    {
         MultiIndex a = single_to_multiindex(i, grid.node_count_per_dimension());
         entry_counter = 1;
-        if (grid.is_boundary_node(i)) {
-            m.row_nz_index(i,0) = i;
-            m.row_nz_entry(i,0) = 1;
-            //v.push_back(std::make_tuple(i, i, 1));
-            b[i] = boundary_function(grid.node_coordinates(i));
+        if(grid.is_boundary_node(grid.partition().to_global_index(i)))
+        {
+            m.row_nz_index(i, 0) = i;
+            m.row_nz_entry(i, 0) = 1;
+            b[i] = boundary_function(grid.node_coordinates(grid.partition().to_global_index(i)));
             continue;
         }
-        b[i] = rhs_function(grid.node_coordinates(i));
+        b[i] = rhs_function(grid.node_coordinates(grid.partition().to_global_index(i)));
         T uii_sum = 0;
-        for (int j = 0; j < space_dimension; ++j) {
+        for(int j = 0; j < space_dimension; ++j)
+        {
             T h2 = grid.node_neighbor_distance(i, j, NeighborSuccession::successor);
             h2 *= h2;
             uii_sum += 2 / h2;
             MultiIndex a_ = a;
             a_[j] += 1;
             int i_ = multi_to_single_index(a_, grid.node_count_per_dimension());
-            m.row_nz_index(i,entry_counter) = i_;
-            m.row_nz_entry(i,entry_counter) = (-1) / h2;
+            m.row_nz_index(i, entry_counter) = i_;
+            m.row_nz_entry(i, entry_counter) = (-1) / h2;
             entry_counter += 1;
-            //v.push_back(std::make_tuple(i, i_, (-1) / h2));
             a_ = a;
             a_[j] -= 1;
             i_ = multi_to_single_index(a_, grid.node_count_per_dimension());
-            m.row_nz_index(i,entry_counter) = i_;
-            m.row_nz_entry(i,entry_counter) = (-1) / h2;
+            m.row_nz_index(i, entry_counter) = i_;
+            m.row_nz_entry(i, entry_counter) = (-1) / h2;
             entry_counter += 1;
-            //v.push_back(std::make_tuple(i, i_, (-1) / h2));
         }
-        m.row_nz_index(i,0) = i;
-        m.row_nz_entry(i,0) = uii_sum;
+        m.row_nz_index(i, 0) = i;
+        m.row_nz_entry(i, 0) = uii_sum;
         //v.push_back(std::make_tuple(i, i, uii_sum));
     }
     //SparseMatrix<T> m(grid.number_of_nodes(), grid.number_of_nodes(), v);
 
-    for (int i = 0; i < grid.number_of_nodes(); ++i) {
-        if (!grid.is_boundary_node(i)) {
+    for(int i = 0; i < grid.partition().local_size(); ++i)
+    {
+        if(!grid.is_boundary_node(grid.partition().to_global_index(i)))
+        {
             continue;
         }
 
         std::array<std::pair<int, int>, space_dimension> neighbors;
         grid.neighbors_of(i, neighbors);
-        for (int j = 0; j < space_dimension; ++j) {
-            for (int k = 0; k < 2; ++k) {
+        for(int j = 0; j < space_dimension; ++j)
+        {
+            for(int k = 0; k < 2; ++k)
+            {
                 int index = k == 0 ? neighbors[j].first : neighbors[j].second;
-                if (index != -1 && !grid.is_boundary_node(index)) {
-                    for (int l = 0; l < m.row_nz_size(index); ++l) {
-                        if (i == m.row_nz_index(index, l)) {
-                            b[index] -= m.row_nz_entry(index, l) * boundary_function(grid.node_coordinates(i));
-                            m.row_nz_entry(index, l) = 0;
+                if(index != -1 && !grid.is_boundary_node(index))
+                {
+                    for(int l = 0; l < m.row_nz_size(grid.partition().to_local_index(index)); ++l)
+                    {
+                        if(i == m.row_nz_index(grid.partition().to_local_index(index), l))
+                        {
+                            b[grid.partition().to_local_index(index)] -= m.row_nz_entry(grid.partition().to_local_index(index), l) * boundary_function(grid.node_coordinates(i));
+                            m.row_nz_entry(grid.partition().to_local_index(index), l) = 0;
                         }
                     }
                 }
