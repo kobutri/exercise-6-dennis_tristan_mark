842dc1f0e13cfac9491f50aaf9bbc20c10faa6ef
DennisWt <dennis@wollgast.net>
2021-02-10 16:44:35 +0100

    initialize neighbors with 0

--- linear_algebra/exchangepattern.h
+++ linear_algebra/exchangepattern.h
@@ -45,7 +45,7 @@ inline ExchangePattern
 create_exchange_pattern(const SparseMatrix<T>& matrix, const ContiguousParallelPartition& column_partition)
 {
     int number_of_processes = column_partition.owner_process(matrix.columns() - 1) + 1;
-    std::vector<int> neighboring_processes(number_of_processes);
+    std::vector<int> neighboring_processes(number_of_processes,0);
     std::vector<std::vector<int>> receive_indices(number_of_processes);
     std::vector<std::vector<int>> send_indices(number_of_processes);
     //int process = column_partition.process();
