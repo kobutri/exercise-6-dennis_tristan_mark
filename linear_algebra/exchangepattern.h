d18a193fed56ae5a723ac8b9a3b28997b389e867
DennisWt <dennis@wollgast.net>
2021-02-10 16:53:39 +0100

    removed -1

--- linear_algebra/exchangepattern.h
+++ linear_algebra/exchangepattern.h
@@ -46,8 +46,8 @@ create_exchange_pattern(const SparseMatrix<T>& matrix, const ContiguousParallelP
 {
     int number_of_processes = column_partition.owner_process(matrix.columns() - 1) + 1;
     std::vector<int> neighboring_processes(number_of_processes, 0);
-    std::vector<std::vector<int>> receive_indices(number_of_processes, -1);
-    std::vector<std::vector<int>> send_indices(number_of_processes, -1);
+    std::vector<std::vector<int>> receive_indices(number_of_processes);
+    std::vector<std::vector<int>> send_indices(number_of_processes);
     //int process = column_partition.process();
     int process_size = column_partition.local_size();
     int owner_process = 0;
